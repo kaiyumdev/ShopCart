d330be4723cff67ca0899396b89d8d345ba14592
kaiyumdev <afrankaiyum34@gmail.com>
2025-02-19 23:22:12 +0600

    feat(icon): add only icon to the navbar

--- src/components/NavItems.jsx
+++ src/components/NavItems.jsx
@@ -87,11 +87,6 @@ const NavItems = () => {
                     user && <li className="nav-item position-relative">
                       <Link to="/cart-page" className="nav-link position-relative d-flex align-items-center">
                         <i className="icofont-cart fs-3 text-dark"></i>
-                        {cartItems?.length > 0 && (
-                          <span className="top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                            {cartItems.length}
-                          </span>
-                        )}
                       </Link>
                     </li>
                   }
